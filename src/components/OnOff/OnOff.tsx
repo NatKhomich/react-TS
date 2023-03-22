2b55edd1dac3031f84a008357c1c8293baf71d38
NatKhomich <yalapulya94@mail.ru>
2023-03-22 11:56:10 +0600

    first commit

---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type OnOffType = {
+    on: boolean
+}
+
+const OnOff = (props: OnOffType) => {
+    return (
+        <div>
+            <div>  </div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default OnOff;
