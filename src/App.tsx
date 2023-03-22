7910cb27b54dd6f838919d4c5dde36b72127454a
NatKhomich <yalapulya94@mail.ru>
2023-03-22 16:01:56 +0600

    uncontrolled components. useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Rating} from './components/Rating/Rating';
 import Accordion from './components/Accordion/Accordion';
 import OnOff from './components/OnOff/OnOff';
+import UncontrolledAccordion from './components/Accordion/UncontrolledAccordion';
+import {UncontrolledRating} from './components/Rating/UncontrolledRating';
 
 function App() {
     return (
-        <div>
-            <OnOff />
-            <OnOff />
-            <OnOff />
+        <div className={'App'}>
+
             <OnOff />
 
-            {/*<Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
+            <Accordion titleValue={'--User--'} collapsed={false} />
+            <UncontrolledAccordion titleValue={'--Menu--'} />
 
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>*/}
-
+            <Rating value={3} />
+            <UncontrolledRating />
         </div>
     );
 }
